3b4aa805d4f6d613ec323b91becb3f78fec07094
lcj <lukas.c.jarren@outlook.de>
2021-04-15 22:37:40 +0200

    added nucleotide complementation with tests

--- make_reverse_complement_2.py
+++ make_reverse_complement_2.py
@@ -9,4 +9,25 @@ class ReverComp:
         if nuc == 'G':
             return 'C'
         else:
-            raise ValueError(f"make_G_complementary received false nucleotide: {nuc}")
+            raise ValueError(f"make_G_complementary received wrong nucleotide: {nuc}")
+
+    @staticmethod
+    def make_C_complementary(nuc) -> str:
+        if nuc == 'C':
+            return 'G'
+        else:
+            raise ValueError(f"make_C_complementary received wrong nucleotide: {nuc}")
+
+    @staticmethod
+    def make_A_complementary(nuc) -> str:
+        if nuc == 'A':
+            return 'T'
+        else:
+            raise ValueError(f"make_A_complementary received wrong nucleotide: {nuc}")
+
+    @staticmethod
+    def make_T_complementary(nuc) -> str:
+        if nuc == 'T':
+            return 'A'
+        else:
+            raise ValueError(f"make_T_complementary received wrong nucleotide: {nuc}")
--- tests/test_reverting_and_complementing_sequence.py
+++ tests/test_reverting_and_complementing_sequence.py
@@ -28,4 +28,47 @@ class test_rc_converting_skills(unittest.TestCase):
         with self.assertRaises(ValueError):
             self.rever_comp.make_G_complementary(nuc=wrong_nucleotide)
 
+    def test_if_C_complementation_works_with_arg(self):
+        # Assume
+        nucleotide = 'C'
+        # Action
+        complementary_nucleotide = self.rever_comp.make_C_complementary(nuc=nucleotide)
+        # Assert
+        self.assertEqual(complementary_nucleotide, 'G')
 
+    def test_if_C_complementation_raise_value_error_by_passing_down_wrong_nucleotide(self):
+        # Assume
+        wrong_nucleotide = 'A'
+        # Action & Assert
+        with self.assertRaises(ValueError):
+            self.rever_comp.make_C_complementary(nuc=wrong_nucleotide)
+
+    def test_if_A_complementation_works_with_arg(self):
+        # Assume
+        nucleotide = 'A'
+        # Action
+        complementary_nucleotide = self.rever_comp.make_A_complementary(nuc=nucleotide)
+        # Assert
+        self.assertEqual(complementary_nucleotide, 'T')
+
+    def test_if_A_complementation_raise_value_error_by_passing_down_wrong_nucleotide(self):
+        # Assume
+        wrong_nucleotide = 'G'
+        # Action & Assert
+        with self.assertRaises(ValueError):
+            self.rever_comp.make_A_complementary(nuc=wrong_nucleotide)
+
+    def test_if_T_complementation_works_with_arg(self):
+        # Assume
+        nucleotide = 'T'
+        # Action
+        complementary_nucleotide = self.rever_comp.make_T_complementary(nuc=nucleotide)
+        # Assert
+        self.assertEqual(complementary_nucleotide, 'A')
+
+    def test_if_T_complementation_raise_value_error_by_passing_down_wrong_nucleotide(self):
+        # Assume
+        wrong_nucleotide = 'G'
+        # Action & Assert
+        with self.assertRaises(ValueError):
+            self.rever_comp.make_T_complementary(nuc=wrong_nucleotide)
